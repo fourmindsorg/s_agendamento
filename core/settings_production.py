0ce58ebc7a51220ee5b869e014f98b7437deda1b
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2025-10-30 09:12:36 -0300

    feat: enforce DB sslmode=require and fetch DB creds from AWS SSM (SSH + SSM)

--- core/settings_production.py
+++ core/settings_production.py
@@ -46,6 +46,10 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
+        "OPTIONS": {
+            # Enforce SSL in production by default
+            "sslmode": os.environ.get("DB_SSLMODE", "require"),
+        },
     }
 }
 
